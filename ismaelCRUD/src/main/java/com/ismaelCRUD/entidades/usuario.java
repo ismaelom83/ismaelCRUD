29abf0a7a36fbdb57225572520c88d65485d7f3a
ismael <ismaelom83@gmail.com>
2020-03-06 13:19:38 +0100

    entidaes creadas

---
+++ ismaelCRUD/src/main/java/com/ismaelCRUD/entidades/usuario.java
@@ -0,0 +1,217 @@
+package com.ismaelCRUD.entidades;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Transient;
+import javax.persistence.JoinColumn;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class usuario implements Serializable {
+
+	/**
+	 * 
+	 */
+	
+	//entidad
+	private static final long serialVersionUID = 6989612429797393220L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO, generator="native")
+	@GenericGenerator(name="native",strategy="native")
+	private long id;
+	@Column
+	private String nombre;
+	@Column
+	private String apellidos;
+	@Column
+	private String email;
+	@Column
+	private String nombreusuario;
+	@Column
+	private String password;
+	@Transient
+	private String confirmacionPassword;
+
+	
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name="user_roles"
+		,joinColumns=@JoinColumn(name="user_id")
+		,inverseJoinColumns=@JoinColumn(name="role_id"))
+	private Set<Roles> roles;
+
+
+	public usuario(long id) {
+		super();
+		this.id = id;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getNombreusuario() {
+		return nombreusuario;
+	}
+
+
+	public void setNombreusuario(String nombreusuario) {
+		this.nombreusuario = nombreusuario;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getConfirmacionPassword() {
+		return confirmacionPassword;
+	}
+
+
+	public void setConfirmacionPassword(String confirmacionPassword) {
+		this.confirmacionPassword = confirmacionPassword;
+	}
+
+
+	public Set<Roles> getRoles() {
+		return roles;
+	}
+
+
+	public void setRoles(Set<Roles> roles) {
+		this.roles = roles;
+	}
+
+	
+	
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((apellidos == null) ? 0 : apellidos.hashCode());
+		result = prime * result + ((confirmacionPassword == null) ? 0 : confirmacionPassword.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((nombreusuario == null) ? 0 : nombreusuario.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((roles == null) ? 0 : roles.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		usuario other = (usuario) obj;
+		if (apellidos == null) {
+			if (other.apellidos != null)
+				return false;
+		} else if (!apellidos.equals(other.apellidos))
+			return false;
+		if (confirmacionPassword == null) {
+			if (other.confirmacionPassword != null)
+				return false;
+		} else if (!confirmacionPassword.equals(other.confirmacionPassword))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (nombreusuario == null) {
+			if (other.nombreusuario != null)
+				return false;
+		} else if (!nombreusuario.equals(other.nombreusuario))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (roles == null) {
+			if (other.roles != null)
+				return false;
+		} else if (!roles.equals(other.roles))
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "usuario [id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", email=" + email
+				+ ", nombreusuario=" + nombreusuario + ", password=" + password + ", confirmacionPassword="
+				+ confirmacionPassword + ", roles=" + roles + "]";
+	}
+	
+	
+	
+}
