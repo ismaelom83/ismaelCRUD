3e772ded161cdd6ace066aeb52636bb00a3e0477
Formacion <Formacion@serbatic-05947.VASS.INET>
2020-04-12 03:01:09 +0200

    cambios en css y html

--- ismaelCRUD/src/main/resources/static/js/index.js
+++ ismaelCRUD/src/main/resources/static/js/index.js
@@ -5,9 +5,70 @@
   "lengthMenu": [ [5, 10, 25, 50, -1], [5, 10, 25, 50, "All"] ],
   "language":idioma_espanol
 } );
-		});
+        });
 
-//esta variable es para cambiar el idima al español		
+               //funciones para mostar los enlaces del silder
+          $("#demo").mouseenter(function () {   
+         $("#e1").fadeIn(1000);
+           $("#d1").fadeIn(1000);
+    })
+
+                 //funciones para ocultar los enlaces del slider
+          $("#demo").mouseleave(function () {
+       $("#e1").fadeOut(1000);
+           $("#d1").fadeOut(1000);
+    })
+
+             $("#demo").mouseenter(function () {
+       $("#e2").fadeIn(1000);
+           $("#d2").fadeIn(1000);
+    })
+
+            $("#demo").mouseleave(function () {
+      ("#e2").fadeOut(1000);
+           $("#d2").fadeOut(1000);
+    })
+
+             $("#demo").mouseenter(function () {
+       $("#e3").fadeIn(1000);
+           $("#d3").fadeIn(1000);
+    })
+
+            $("#demo").mouseleave(function () {
+       ("#e3").fadeOut(1000);
+           $("#d3").fadeOut(1000);
+    })
+
+  
+   
+
+        //funciones para cerrar el modal
+        
+          $("#modalmio").click(function () {
+        $("#springSecurity").modal('hide');
+    })
+            $("#modalmio2").click(function () {
+        $("#myGitHub").modal('hide');
+    })
+
+        //funciones para mostrar los modales
+            function modalSpringSecurity() {
+      $('#springSecurity').modal('show');
+    }
+
+      function gitHub() {
+      $('#myGitHub').modal('show');
+    }
+
+
+//funcion para modal sobre mi
+
+function modalSobreMi(){
+    $("#sobreMi").modal('show');
+}
+
+
+//esta variable es para cambiar el idima al español	de la libreria de datatable
 		var idioma_espanol = {
     "sProcessing":     "Procesando...",
                 "sLengthMenu":     "Mostrar _MENU_ registros",
